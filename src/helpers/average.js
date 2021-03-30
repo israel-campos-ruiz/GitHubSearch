db15413875c086767fbc216c49e29d1883a0c9ac
Israel Campos Ruiz <israel.campos@ugd.gob.mx>
2021-03-30 13:00:48 -0600

    se añaden los helpers

---
+++ src/helpers/average.js
@@ -0,0 +1,8 @@
+export const average =  (props) => {
+    let total = 0;  
+    
+    const sum = Object.entries(props).map(([name, value]) =>  total += value)
+    const totalSum = sum[sum.length-1];
+    const promedio = Object.entries(props).map(([name,value])=> value = (value *100) / totalSum);
+    return promedio
+}
---
+++ src/helpers/nameChart.js
@@ -0,0 +1,2 @@
+export const nameCharst = (props) => Object.entries(props).map(([name, value]) =>  name)
+
