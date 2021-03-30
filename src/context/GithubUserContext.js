efc5a9f6bd0b9ff4d8978bcc103d9a093950c7d9
Israel Campos Ruiz <israel.campos@ugd.gob.mx>
2021-03-30 13:53:29 -0600

    se arregla un bug en la peticion del context

--- src/context/GithubUserContext.js
+++ src/context/GithubUserContext.js
@@ -3,12 +3,10 @@ import useGet from '../hooks/useGet';
 export const UserContext = createContext();
 const GithubUserContextProvider = ({ children }) => {
   const [searchUser, setSearchUser] = useState('israel-campos-ruiz');
-  // const [followersData, setfirstFollower] = useState([]);
   const [firstRepoName, setFirstRepoName] = useState(null);
   const { data, loading, error } = useGet(
     `https://api.github.com/users/${searchUser}`
   );
-  // const state = useGet(`${data?.followers_url}`);
   const repoName = useGet(
     `https://api.github.com/users/${searchUser}/repos?per_page=1`
   );
@@ -18,13 +16,6 @@ const GithubUserContextProvider = ({ children }) => {
       setFirstRepoName(repoName.data[0].name);
     }
   }, [repoName]);
-
-  // useEffect(()=>{
-  //     if(data?.followers_url){
-  //       setfirstFollower(state.data)
-  //     }
-  // },[data?.followers_url, state.data])
-
   return (
     <UserContext.Provider
       value={{
